7545818e1d42e3dfc4c9e6d671fca5bc9dcfac90
syrauff <syahrul1_s1sisfo@mahasiswa.ung.ac.id>
2026-05-18 13:04:25 +0800

    fix: increase workbox max file size limit to fix netlify build

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,7 @@ export default defineConfig({
         ]
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
         additionalManifestEntries: [
           { url: '/model/model.json', revision: null },
